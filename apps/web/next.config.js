9d99623ce0e4f55a4e6341d46548ab75083a093d
n0safe <sssebillemathis@gmail.com>
2024-07-04 14:55:08 +0200

    update some config in the next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,4 +4,8 @@
 module.exports = {
     reactStrictMode: true,
     transpilePackages: ["@repo/ui"],
+    images: {
+        dangerouslyAllowSVG: true,
+        domains: [process.env.NEXT_PUBLIC_API_HOSTNAME]
+    }
 };
