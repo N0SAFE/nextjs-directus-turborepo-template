246c55ff82a5575e92872204f61a11f8eb646b95
Mathis Sebille <mathis.sebille@affluences.com>
2025-01-15 14:57:45 +0100

    refactor: remove unused Directus commands and bindings; update .gitignore for clarity

--- packages/ui/components/theme-provider.tsx
+++ packages/ui/components/theme-provider.tsx
@@ -1,7 +1,16 @@
 'use client'
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider as StaticNextThemesProvider } from 'next-themes'
+
+import dynamic from 'next/dynamic'
+
+const NextThemesProvider =
+    process.env.NODE_ENV === 'production'
+        ? StaticNextThemesProvider
+        : dynamic(() => import('next-themes').then((e) => e.ThemeProvider), {
+              ssr: false,
+          })
 
 export default function ThemeProvider({
     children,
