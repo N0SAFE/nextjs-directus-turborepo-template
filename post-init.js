463afd878e6449c5d8a9bf45fa574034f019c495
Mathis Sebille <mathis.sebille@affluences.com>
2025-02-06 14:56:41 +0100

    feat: ensure .env file is created if it does not exist and clean up empty lines in environment variables

--- post-init.js
+++ post-init.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 
 fs.writeFileSync(".initiated", "");
+if (!fs.existsSync(".env")) {
+    fs.writeFileSync(".env", "");
+}
 const data = fs.readFileSync('.env').toString()
-const parsed = data.split('\n').map((line) => line.split('=')
+const parsed = data.split('\n').filter((line) => line !== '').map((line) => line.split('=')
 ).filter(([key, v]) => !key.startsWith('TEMP') && key && v).map(([key, v]) => `${key}=${v}`).join('\n')
 fs.writeFileSync('.env', parsed)
